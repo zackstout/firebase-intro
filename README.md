ed79cd26304493d9d5e74889926a4ee3b2075909
Zack Stout <zackstout@gmail.com>
2018-03-27 02:10:15 -0500

    very strange conundrums about file sourcing vs all in one file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Intro to Firebase
+Thanks a ton to [Luke](https://github.com/LukeSchlangen) for his [starter video](https://www.youtube.com/watch?v=_2kaJ4Kxc6E).
--- client.js
+++ client.js
@@ -1,9 +1,33 @@
 
-var header = document.getElementById('header');
 
+
+var header = document.getElementById('header');
+console.log(header);
 var dbRef = firebase.database().ref().child('header');
 
+// Why can't we see these logs?!?
+console.log('hi');
 dbRef.on('value', function(snap) {
   console.log(snap);
+  console.log('hi');
+
   header.innerText = snap.val();
+});
+
+var signIn = document.getElementById("signIn");
+var signOut = document.getElementById("signOut");
+console.log(signIn);
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+signIn.addEventListener('click', function() {
+  console.log('clickin');
+  firebase.auth().signInWithPopUp(provider).then(function(user) {
+    if (user) {
+      // dbRef.on('value', function(snap) {
+      //   console.log(snap);
+      //   header.innerText = snap.val();
+      // });
+    }
+  });
 });

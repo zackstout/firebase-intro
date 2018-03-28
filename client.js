6521167c6c0095c6fb39ae07f6cc3ea847aeed45
Zack Stout <zackstout@gmail.com>
2018-03-28 00:29:01 -0500

    ahh yes we had to do it all in the index.html

--- client.js
+++ client.js
@@ -1,33 +1,39 @@
-
-
-
-var header = document.getElementById('header');
-console.log(header);
-var dbRef = firebase.database().ref().child('header');
-
-// Why can't we see these logs?!?
-console.log('hi');
-dbRef.on('value', function(snap) {
-  console.log(snap);
-  console.log('hi');
-
-  header.innerText = snap.val();
-});
-
-var signIn = document.getElementById("signIn");
-var signOut = document.getElementById("signOut");
-console.log(signIn);
-
-var provider = new firebase.auth.GoogleAuthProvider();
-
-signIn.addEventListener('click', function() {
-  console.log('clickin');
-  firebase.auth().signInWithPopUp(provider).then(function(user) {
-    if (user) {
-      // dbRef.on('value', function(snap) {
-      //   console.log(snap);
-      //   header.innerText = snap.val();
-      // });
-    }
-  });
-});
+//
+// var header = document.getElementById('header');
+// console.log(header);
+// var dbRef = firebase.database().ref().child('header');
+//
+// // Why can't we see these logs?!?
+// console.log('hi');
+// dbRef.on('value', function(snap) {
+//   console.log(snap);
+//   console.log('hi');
+//
+//   header.innerText = snap.val();
+// });
+//
+// var signIn = document.getElementById("signIn");
+// var signOut = document.getElementById("signOut");
+// console.log(signIn);
+//
+// var provider = new firebase.auth.GoogleAuthProvider();
+//
+// signIn.addEventListener('click', function() {
+//   console.log('clickin');
+//   firebase.auth().signInWithPopUp(provider).then(function(user) {
+//     if (user) {
+//       dbRef.on('value', function(snap) {
+//         console.log(snap);
+//         header.innerText = snap.val();
+//       });
+//     }
+//   });
+// });
+//
+// signOut.addEventListener('click', function() {
+//   firebase.auth().signOut().then(function() {
+//     header.innerText = "sign in homie!";
+//   });
+// });
+//
+// var playground = document.getElementById('playground');
